95e7b4fdc78f8d48e8505c70bc6a6305c47afe4e
Lázaro Cáceres <melanie16203@gmail.com>
2023-02-26 11:15:01 -0300

    redirect to thanks page

--- components/Header.js
+++ components/Header.js
@@ -20,17 +20,17 @@ export default function Header() {
                 </div>
                 <div className='hidden lg:flex gap-4 items-center'>
                     <a
-                        href='https://dashboard.rulelogic.io/account/sign-up'
+                        href='https://dashboard.rulelogic.io/account/sign-in'
                         className='py-[0.55rem] px-4 border-2 border-gray-400 border-opacity-60 rounded-full font-bold block hover:scaled hover:border-neutral-800 transition-all'
                     >
                         Log In
                     </a>
-                    <a
-                        href='https://dashboard.rulelogic.io/account/sign-in'
+                    <Link
+                        href='/thanksForRegistering'
                         className='py-[0.46rem] block px-7 bg-primary hover:bg-opacity-90 text-white border border-primary border-opacity-60 hover:border-opacity-90 hover:scaled transition-all rounded-full font-bold'
                     >
                         Register
-                    </a>
+                    </Link>
                 </div>
                 <MobileNav />
             </div>
